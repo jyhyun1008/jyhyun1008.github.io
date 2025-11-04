f04e1363df891ba7bbdab5e22a1649c28b6adfb9
jyhyun1008 <jayhyun1008@gmail.com>
2025-11-04 13:09:42 +0900

    add SEO meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,32 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/content'],
   ssr: true,
+  app: {
+    head: {
+      title: 'HoweverIna Studio Blog', // default fallback title
+      htmlAttrs: {
+        lang: 'ko',
+      },
+      meta: [
+        { name: 'author', content: 'However Ina' },
+        { name: 'keywords', content: 'music, vtuber' },
+        { name: 'robots', content: 'all' },
+        { name: 'description', content: '연이나의 커미션/외주 샘플, 그리고 기술 블로그' },
+        { property: 'og:url', content: 'https://blog.howeverina.studio' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'HoweverIna Studio Blog' },
+        { property: 'og:description', content: '연이나의 커미션/외주 샘플, 그리고 기술 블로그' },
+        { property: 'og:image', content: '/favicon.png' },
+        { property: 'twitter:card', content: 'summary' },
+        { property: 'twitter:title', content: 'HoweverIna Studio Blog' },
+        { property: 'twitter:description', content: '연이나의 커미션/외주 샘플, 그리고 기술 블로그' },
+        { property: 'twitter:image', content: '/favicon.png' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   routeRules: {
     '/blog/**': { prerender: true }
   },
