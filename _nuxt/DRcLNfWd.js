ed3db7b154bbb8a72aca4639ba606c3a06c9289c
jyhyun1008 <jayhyun1008@gmail.com>
2025-11-06 15:08:54 +0900

    Updates

---
+++ _nuxt/DRcLNfWd.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,o as r,H as n}from"./CC7-6MVi.js";const t={};function c(e,a){return r(),o("p",null,[n(e.$slots,"default")])}const l=Object.assign(s(t,[["render",c]]),{__name:"ProseP"});export{l as default};
---
+++ _nuxt/lwFJts4S.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./CjNCwaY6.js";import{G as n,o as s,w as f,H as p}from"./CC7-6MVi.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
