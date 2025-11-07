89bf79b8cfd0f7acfb949bae3747c9a3692a4f1a
jyhyun1008 <jayhyun1008@gmail.com>
2025-11-07 16:25:45 +0900

    Updates

---
+++ _nuxt/Ca1p_V1N.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,o as r,H as t}from"./fPuvPo-E.js";const n={};function c(e,a){return r(),s("tbody",null,[t(e.$slots,"default")])}const l=Object.assign(o(n,[["render",c]]),{__name:"ProseTbody"});export{l as default};
